3de0bdcd2f61638f527b13efa78850bce6201001
Reid Liu <Reid.liu@kuka.com>
2022-03-23 16:03:16 +0800

    add code error example

--- code_error_example/src/main/java/com/fantasybaby/dee/code/oom/groovyoom/GroovyMistakesApplication.java
+++ code_error_example/src/main/java/com/fantasybaby/dee/code/oom/groovyoom/GroovyMistakesApplication.java
@@ -1,6 +1,5 @@
-package com.fantasybaby.dee.code.oom.impropermaxheadersize;
+package com.fantasybaby.dee.code.oom.groovyoom;
 
-import com.fantasybaby.dee.common.Utils;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +7,12 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-
-@SpringBootApplication(scanBasePackages = "com.fantasybaby.dee.code.oom.impropermaxheadersize"
+@SpringBootApplication(scanBasePackages = "com.fantasybaby.dee.code.oom.groovyoom"
         , exclude = { RedissonAutoConfiguration.class, RedisAutoConfiguration.class, DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class CommonMistakesApplicationBad {
+public class GroovyMistakesApplication {
+
     public static void main(String[] args) {
-        Utils.loadPropertySource(CommonMistakesApplicationBad.class, "bad.properties");
-        SpringApplication.run(CommonMistakesApplicationBad.class, args);
+        SpringApplication.run(GroovyMistakesApplication.class, args);
     }
 }
 
--- code_error_example/src/main/java/com/fantasybaby/dee/code/oom/impropermaxheadersize/CommonMistakesApplicationGood.java
+++ code_error_example/src/main/java/com/fantasybaby/dee/code/oom/impropermaxheadersize/CommonMistakesApplicationGood.java
@@ -2,11 +2,16 @@ package com.fantasybaby.dee.code.oom.impropermaxheadersize;
 
 import com.fantasybaby.dee.common.Utils;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fantasybaby.dee.code.oom.impropermaxheadersize"
+        , exclude = { RedissonAutoConfiguration.class, RedisAutoConfiguration.class, DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @Slf4j
 public class CommonMistakesApplicationGood {
     public static void main(String[] args) {
