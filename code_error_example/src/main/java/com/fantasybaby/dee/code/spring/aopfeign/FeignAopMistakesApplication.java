6d81858c1833dcf60f44430d75eae5a67ebb7351
fantasybaby <625353861@qq.com>
2022-04-05 17:22:36 +0800

    add standard property

--- code_error_example/src/main/java/com/fantasybaby/dee/code/spring/aopfeign/FeignAopMistakesApplication.java
+++ code_error_example/src/main/java/com/fantasybaby/dee/code/spring/aopfeign/FeignAopMistakesApplication.java
@@ -2,11 +2,14 @@ package com.fantasybaby.dee.code.spring.aopfeign;
 
 import com.fantasybaby.dee.common.Utils;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(scanBasePackages = "com.fantasybaby.dee.code.spring.aopfeign"
         , exclude = { RedissonAutoConfiguration.class, RedisAutoConfiguration.class, DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
@@ -14,7 +17,12 @@ public class FeignAopMistakesApplication {
 
     public static void main(String[] args) {
         Utils.loadPropertySource(FeignAopMistakesApplication.class, "feign.properties");
-        SpringApplication.run(FeignAopMistakesApplication.class, args);
+//        SpringApplication.run(FeignAopMistakesApplication.class, args);
+        new SpringApplicationBuilder(FeignAopMistakesApplication.class)
+                // .REACTIVE, .SERVLET
+                .web(WebApplicationType.NONE)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
     }
 }
 
--- code_error_example/src/main/java/com/fantasybaby/dee/code/spring/propertysource/StandardPropertySourceApplication.java
+++ code_error_example/src/main/java/com/fantasybaby/dee/code/spring/propertysource/StandardPropertySourceApplication.java
@@ -1,23 +1,34 @@
 package com.fantasybaby.dee.code.spring.propertysource;
 
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.StandardEnvironment;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fantasybaby.dee.code.spring.propertysource"
+        , exclude = {RedissonAutoConfiguration.class, RedisAutoConfiguration.class, DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @Slf4j
-public class CommonMistakesApplication {
+public class StandardPropertySourceApplication {
     @Autowired
     private StandardEnvironment env;
 
     public static void main(String[] args) {
         //环境变量添加 MANAGEMENT_SERVER_PORT=12345 MANAGEMENT_SERVER_IP=192.168.0.2
-        SpringApplication.run(CommonMistakesApplication.class, args);
+        new SpringApplicationBuilder(StandardPropertySourceApplication.class)
+                // .REACTIVE, .SERVLET
+                .web(WebApplicationType.NONE)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
     }
 
     @PostConstruct
