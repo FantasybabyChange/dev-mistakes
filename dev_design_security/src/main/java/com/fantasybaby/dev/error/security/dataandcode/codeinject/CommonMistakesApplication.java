0037ca4b8d99f02e4f0702f30c40b983e74dda7c
fantasybaby <625353861@qq.com>
2022-06-12 21:22:33 +0800

    run the work

--- dev_design_security/src/main/java/com/fantasybaby/dev/error/security/dataandcode/codeinject/CommonMistakesApplication.java
+++ dev_design_security/src/main/java/com/fantasybaby/dev/error/security/dataandcode/codeinject/CommonMistakesApplication.java
@@ -2,9 +2,12 @@ package com.fantasybaby.dev.error.security.dataandcode.codeinject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fantasybaby.dev.error.security.dataandcode.codeinject"
+,exclude = {HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class CommonMistakesApplication {
 
     public static void main(String[] args) {
