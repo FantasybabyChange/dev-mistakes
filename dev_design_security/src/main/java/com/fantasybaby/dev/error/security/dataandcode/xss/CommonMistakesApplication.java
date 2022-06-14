ad6250a084617370daee7cfb2ad6d9721c2ec044
Reid Liu <Reid.liu@kuka.com>
2022-06-14 18:15:33 +0800

    add AES

--- dev_design_security/src/main/java/com/fantasybaby/dev/error/security/dataandcode/xss/CommonMistakesApplication.java
+++ dev_design_security/src/main/java/com/fantasybaby/dev/error/security/dataandcode/xss/CommonMistakesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fantasybaby.dev.error.security.dataandcode.xss")
 public class CommonMistakesApplication {
 
     public static void main(String[] args) {
---
+++ dev_design_security/src/main/java/com/fantasybaby/dev/error/security/sensitivedata/storeidcard/CipherResult.java
@@ -0,0 +1,9 @@
+package com.fantasybaby.dev.error.security.sensitivedata.storeidcard;
+
+import lombok.Data;
+
+@Data
+public class CipherResult {
+    private Long id;
+    private String cipherText;
+}
