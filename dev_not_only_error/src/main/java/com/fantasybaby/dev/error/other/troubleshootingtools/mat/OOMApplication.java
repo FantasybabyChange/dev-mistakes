c063d4fe2d9b98b7cfc1ec370f523a4274ab19e3
Reid Liu <Reid.liu@kuka.com>
2022-06-21 16:36:13 +0800

    test oom and arthas

--- dev_not_only_error/src/main/java/com/fantasybaby/dev/error/other/troubleshootingtools/mat/OOMApplication.java
+++ dev_not_only_error/src/main/java/com/fantasybaby/dev/error/other/troubleshootingtools/mat/OOMApplication.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fantasybaby.dev.error.other.troubleshootingtools.mat",exclude = {DataSourceAutoConfiguration.class,JdbcRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class OOMApplication implements CommandLineRunner {
 
     @Autowired
